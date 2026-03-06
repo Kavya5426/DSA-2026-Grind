5a6c51072c52e9770ffb92208a904898ad113d35
kavya5426 <policepatilkavya77@gmail.com>
2026-03-06 23:35:04 +0530

    Updated files

--- Binary_Search/11_Search_Insert_Position.py
+++ Binary_Search/11_Search_Insert_Position.py
@@ -25,19 +25,3 @@ Space Complexity: O(1)
 - Only constant extra variables are used.
 """
 
-class Solution(object):
-    def search(self, nums, target):
-        left = 0
-        right = len(nums) - 1
-
-        while left <= right:
-            mid = (left + right) // 2 #use '//' in py3 for integer division.'/' gives float result in py3
-
-            if nums[mid] == target:
-                return mid
-            elif target < nums[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return -1
